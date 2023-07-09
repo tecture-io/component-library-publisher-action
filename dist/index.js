ce8e1583666d7341f3c6b435bcb27277f3d6390f
Shanika Wijerathna <shanikacj@gmail.com>
2023-07-09 19:53:30 +1200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+console.log("Hello World!");
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
